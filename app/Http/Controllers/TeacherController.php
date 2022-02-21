edc87e1c4176a106ec648c8d6a777df21778f7d9
Dhanapal <dhanapal.m@cloudvalleytech.com>
2022-02-21 14:57:00 +0530

    latest files

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -15,7 +15,7 @@ class TeacherController extends Controller
     {
         $this->middleware('auth', [
             'except' => [
-                'profile',
+                'profile','add'
 
             ],
         ]);
