aedb1b158c6c54a2a390f95db5f00f9bb9f9c31d
Dhanapal <dhanapal.m@cloudvalleytech.com>
2022-03-29 16:51:00 +0530

    plan ststus checking  updated

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -32,24 +32,35 @@ class ParentController extends Controller
         }
     }
     public function add_kids(Request $request){
-     
+     $no_of_children=DB::table('users')->where('id',$request['user_id'])->select('no_of_children as child')->first();
+     $child_count=$no_of_children->child;
+
          try {
                 if (count($request->input('data')) > 0) {
                     foreach ($request->input('data') as $key => $value) {
-                        
-                        if(DB::table('users')->where('email',$value['email'])->count()==0){
-                             $plainPassword = $value['password'];
-                            DB::table('users')->insert([
-                            'name'=>$value['name'],
-                            'email'=>$value['email'],
-                            'password'=>app('hash')->make($plainPassword),
-                            'parent_id'=>$request['user_id'],
-                            'role'=>5                            
-                            ]);
+                       $added_count=DB::table('users')->where('parent_id',$request['user_id'])->count(); 
+                        #check childcount
+                       if($child_count >= $added_count)
+                       {
+                            if(DB::table('users')->where('email',$value['email'])->count()==0)
+                            {
+                                    $plainPassword = $value['password'];
+                                    DB::table('users')->insert([
+                                    'name'=>$value['name'],
+                                    'email'=>$value['email'],
+                                    'password'=>app('hash')->make($plainPassword),
+                                    'parent_id'=>$request['user_id'],
+                                    'role'=>5                            
+                                    ]);
+                                }
+                                #email exists
+                                else{
+                                    return response()->json(['status' => false, 'message' =>'email exists'], 200);
+                                }
                         }
-                        #email exists
+                        #child countoverlimit
                         else{
-                             return response()->json(['status' => false, 'message' =>'email exists'], 200);
+                            return response()->json(['status' => false, 'message' =>'Already kids added for you package Limit'], 200);
                         }
                     }
                      return response()->json(['status' => true, 'message' =>'Kidz Added Successfully'], 200);
