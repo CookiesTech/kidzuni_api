a3f760c68db29525d33a8967fe8afd361db051a6
Dhanapal <dhanapal.m@cloudvalleytech.com>
2022-07-01 14:33:37 +0530

    search api added

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -22,10 +22,51 @@ class ParentController extends Controller
 
      public function getAllParents()
     {
+        $final_data=[];
         try {
             $data = DB::table('users')->where('role',3)->orderBy('id','desc')->get();
+           
+            if($data){
+                 $current_datetime=date('Y-m-d');
+                foreach($data as $value){
+                    $expiry='';$status='';
+                    $type=$value->subscription_type;
+                    $purchased_date=$value->purchased_datetime;
+                      if($type=='monthly'){
+                             $date =explode(" ",$purchased_date)[0];
+                            $newdate =date("Y-m-d", strtotime ( '+1 month' , strtotime ( $date ) )) ;
+                           
+                            #check plan expired or not 
+                            $diff= strtotime($newdate)-strtotime($current_datetime);          
+                                if($diff >0){
+                                   $status='Active';
+                                   $expiry=$newdate;
+                                }#plan expired
+                                else{
+                                    $status='Expired';
+                                    $expiry=$newdate;
+                                }
+                        }#plan type Annual
+                        else{
+                            $date =explode(" ",$purchased_date)[0];
+                            $newdate = date("Y-m-d", strtotime ( '+12 month' , strtotime ( $date ) )) ;
+                            $diff= strtotime($newdate)-strtotime($current_datetime);    
+                        
+                            if ($diff>0){
+                                $status='Active';
+                                $expiry=$newdate;
+                            }#plan expired
+                            else{
+                                $status='Expired';
+                                $expiry=$newdate;
+                            }
+                        }                       
+                    $final_data[]=array('name'=>$value->name,'email'=>$value->email,'subscription_type'=>$value->subscription_type,
+                    'purchased_datetime'=>$value->purchased_datetime,'status'=>$status,'expiry'=>$expiry,'no_of_children'=>$value->no_of_children);
+                }
+            }
 
-            return response()->json(['status' => true, 'data' => $data], 200);
+            return response()->json(['status' => true, 'data' => $final_data], 200);
         } catch (\Exception $e) {
 
             return response()->json(['status' => false, 'data' => []], 200);
