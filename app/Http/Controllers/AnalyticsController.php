19abec59fbe3c1a2db81a72b390333fdbd463b0e
Dhanapal <dhanapal.m@cloudvalleytech.com>
2022-07-15 16:05:25 +0530

    dsd

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -25,13 +25,13 @@ class AnalyticsController extends Controller
          $standard_id=DB::table('standards')->where('country_code',$request->post('country_code'))->pluck('id');
        
         }else{
-            $standard_id=$request->post('standard_id');
+            $standard_id=[$request->post('standard_id')];
         }
         if($request->post('subject_id')=='All'){
             $subject_id=DB::table('subjects')->where('country_code',$request->post('country_code'))->limit(1)->pluck('id');
            
         }else{
-            $subject_id=$request->post('subject_id');
+            $subject_id=[$request->post('subject_id')];
         }
        $inputDaterange=$request->post('date_range');
        if($inputDaterange=='month')
